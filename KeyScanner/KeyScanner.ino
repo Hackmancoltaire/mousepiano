37e8c4b54c275c36187038795bb77f15c3994676
Ramon Yvarra <ryvarra@apple.com>
2015-07-20 22:12:46 -0700

    Finalist version of KeyScanner
    Keys now have an upper and lower tolerance that is used to create the
    range for the key movements. Keys should all be pressed to establish
    those boundaries.

--- KeyScanner/KeyScanner.ino
+++ KeyScanner/KeyScanner.ino
@@ -1,3 +1,21 @@
+// Hardware: Mega 2560 R2 + Ethernet Shield
+
+#define MOUSEDEBUG 0
+#define LOGICDEBUG 0
+#define VISUALIZER 0
+
+#if defined(ARDUINO) && ARDUINO > 18
+#include <SPI.h>a
+#endif
+
+#include <Ethernet.h>
+#include "AppleMidi.h"
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {
+  0x90, 0xA2, 0xDA, 0x0F, 0x4F, 0xF7
+};
 
 #define FASTADC 1
 
@@ -9,8 +27,13 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
+byte keyUp[88]; // 704 bits
+byte keyDown[88]; // 704 bits
+
 byte keyVelocity[88]; // 704 bits
 byte keyStatus[22]; // 176 bits
+
+// Total memory
 
 // Each keyVelocity has these bits
 // delayTime = 0-127 for velocity (7 bits)
@@ -20,12 +43,14 @@ byte keyStatus[22]; // 176 bits
 // goingDown = boolean for downward motion
 // sent = if midi message sent
 
-byte s0 = 8;
-byte s1 = 9;
-byte s2 = 10;
-byte s3 = 11;
+byte s0 = 22;
+byte s1 = 23;
+byte s2 = 24;
+byte s3 = 25;
 
-byte controlPins[4] = {s0, s1, s2, s3};
+byte controlPins[4] = {
+  s0, s1, s2, s3
+};
 
 byte muxChannel[8] = {
   B00001000, // Channel 0 & 1
@@ -58,47 +83,180 @@ void setup() {
   digitalWrite(s3, LOW);
 
   Serial.begin(115200);
+  Serial.print("Getting IP...");
+
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println();
+    Serial.println( "Failed DHCP!" );
+    for (;;)
+      ;
+  }
+
+  // print your local IP address:
+  Serial.println();
+  Serial.print("IP: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+
+  // Create a session and wait for a remote host to connect to us
+  AppleMIDI.begin("test");
+
+  // Actively connect to a remote host
+  // IPAddress host(192, 168, 2, 1);
+  // AppleMIDI.Invite(host, 5004);
+
+  AppleMIDI.OnConnected(OnAppleMidiConnected);
+  AppleMIDI.OnDisconnected(OnAppleMidiDisconnected);
+
+  AppleMIDI.OnReceiveControlChange(OnAppleMidiControlChange);
+
+  for (byte i = 0; i < 88; i++) {
+    setDownStateForKey(false, i);
+    setGoingDownStateForKey(false, i);
+    setSentStateForKey(true, i);
+  }
 }
 
+byte keyHigh = 180;
+byte keyMid = 100;
+byte keyLow = 80;
+
+int currentPosition = 0;
+int numberOfBoards = 6;
+
+boolean showMessages = false;
+
 void loop() {
+  // Listen to incoming notes
+  AppleMIDI.run();
+
+  if (Serial.available() > 0) {
+    // read the incoming byte
+    int inputByte = Serial.read();
+
+    if (inputByte == 108) {
+      showMessages = !showMessages;
+      Serial.read();
+    } else if (inputByte == 10) {
+      if (currentPosition > 87) {
+        currentPosition = 0;
+      }
+      else {
+        currentPosition++;
+      }
+    } else {
+      Serial.println("Rec: " + String(inputByte));
+    }
+  }
+
   for (byte m = 0; m < 16; m++) {
     switchToChannel(m);
+    delay(2);
 
-    for (byte i = 0; i < 6; i++) {
+    for (byte i = 0; i < numberOfBoards; i++) {
       byte key = (i * 16) + m;
 
       // Don't read the end of the last board
       if (key < 88) {
-        byte keyPosition = map(analogRead(i), 30, 1010, 0, 255);
+        byte keyPosition = map(analogRead(i), 0, 1023, 0, 255);
 
-        if (keyPosition < 200) {
-          if (!downStateForKey(key)) {
-            // setGoingDownStateForKey(true, key)
-            setKeyPosition(key, getKeyPosition(key) + 1);
+        if (keyPosition > keyUp[key]) {
+          keyUp[key] = keyPosition;
+        }
+        if (keyDown[key] == 0) {
+          keyDown[key] = abs(keyUp[key] * 0.8);
+        }
+        if (keyPosition < keyDown[key]) {
+          keyDown[key] = keyPosition;
+        }
 
-            if (keyPosition < 10) {
-              setDownStateForKey(true, key);
-              setSentStateForKey(false, key);
-            }
+        keyPosition = map(keyPosition, keyDown[key], keyUp[key], 0, 255);
+
+        if (key == currentPosition && showMessages) {
+          Serial.println("Channe: " + String(i + 1) + " - Key: " + String(key + 1) + " = " + String(keyPosition + 1) + " - " + String(analogRead(i)) + " / " + String(keyUp[key]) + " - " + String(keyDown[key]));
+        }
+
+        if (VISUALIZER) {
+          Serial.print(String(key) + ":" + String(keyPosition));
+
+          if (key != 87) {
+            Serial.print(",");
           } else {
-            if (keyPosition > 10) {
-              // setGoingDownStateForKey(false, key);
-              setDownStateForKey(false, key);
+            Serial.println();
+          }
+        }
+
+        // If below the height threshold
+        if (keyPosition < keyHigh) {
+          // If not down
+          if (!downStateForKey(key)) {
+            // We are going down
+            setGoingDownStateForKey(true, key);
+
+            // If we're below the key low
+            if (keyPosition < keyLow) {
+              setDownStateForKey(true, key);
+
+              // We need to announace that we're down!
               setSentStateForKey(false, key);
             }
+            else {
+              // Reduce the velocity over time
+              byte currentPosition = getKeyPosition(key);
+              
+              setKeyPosition(key, (currentPosition - 2));
+            }
+          }
+          // We are already down
+          else {
+            // Above mid point and had been going down
+            if (keyPosition > keyMid && goingDownStateForKey(key)) {
+              // We're heading back up
+              setGoingDownStateForKey(false, key);
+              setDownStateForKey(false, key);
+
+              // We need to announce that we're up!
+              setSentStateForKey(true, key);
+            }
+          }
+        }
+        else {
+          // We're above the threshold
+          // If we were down or were going down
+          if (downStateForKey(key) == 1) {
+
+            setGoingDownStateForKey(false, key);
+            setDownStateForKey(false, key);
+
+            // We need to announce that we're up!
+            setSentStateForKey(false, key);
+          } else {
+            setKeyPosition(key, 127);
           }
         }
 
         if (downStateForKey(key)) {
           if (!sentStateForKey(key)) {
             // Send MIDI Note ON with velocity
+            if (MOUSEDEBUG) {
+              Serial.println("Midi ON: " + String(key) + " Velocity:" + getKeyPosition(key));
+            }
+            AppleMIDI.noteOn(key + 24, getKeyPosition(key), 1);
             setSentStateForKey(true, key);
-            setKeyPosition(key, 0);
             setDownStateForKey(true, key);
           }
-        } else {
+        }
+        else {
           if (!sentStateForKey(key)) {
             // Send MIDI Note Off
+            if (MOUSEDEBUG) {
+              Serial.println("Midi OFF: " + String(key) + " Velocity:" + getKeyPosition(key));
+            }
+            AppleMIDI.noteOff(key + 24, 0, 1);
+            setKeyPosition(key, getKeyPosition(key));
             setSentStateForKey(true, key);
           }
         }
@@ -107,50 +265,110 @@ void loop() {
   }
 }
 
+// Zero based channel selector (0-15)
 void switchToChannel(byte channel) {
-  byte block = muxChannel[abs(channel / 2)];
+  byte block = muxChannel[(byte)floor(channel / 2)];
 
-  for (byte i = 0; i < 4; i++) {
-    if (channel % 2 == 0) {
+  if (LOGICDEBUG) {
+    Serial.println("Channel: " + String(channel));
+    Serial.println("Block: " + String((byte)floor(channel / 2)));
+  }
+
+  if (channel % 2 == 0) {
+    for (byte i = 0; i < 4; i++) {
       digitalWrite(controlPins[i], bitRead(block, 7 - i));
-    } else {
-      digitalWrite(controlPins[i], bitRead(block, 3 - i));
+      if (LOGICDEBUG) {
+        Serial.print(bitRead(block, 7 - i));
+      }
     }
+  }
+  else {
+    for (byte i = 0; i < 4; i++) {
+      digitalWrite(controlPins[i], bitRead(block, 3 - i));
+
+      if (LOGICDEBUG) {
+        Serial.print(bitRead(block, 3 - i));
+      }
+    }
+  }
+
+  if (LOGICDEBUG) {
+    Serial.println("");
   }
 }
 
 boolean goingDownStateForKey(byte key) {
-  byte block = abs(key / 4); // key=87, block = 21
-  byte section = key - (block * 4) - 1; // key = 87, section = 2
+  byte section = 0;
+  byte block = byte(abs(key / 4)); // key=87, block = 21
+
+  if (block > 0) {
+    section = byte(key - (block * 4)); // key = 87, section = 2
+  } else {
+    section = key;
+  }
 
   return bitRead(keyStatus[block], (section * 2));
 }
 
 void setGoingDownStateForKey(boolean state, byte key) {
-  byte block = abs(key / 4); // key=87, block = 21
-  byte section = key - (block * 4) - 1; // key = 87, section = 2
+  byte block = 0;
+  byte section = 0;
+
+  block = byte(abs(key / 4)); // key=87, block = 21
+
+  if (block > 0) {
+    section = byte(key - (block * 4)); // key = 87, section = 2
+  } else {
+    section = key;
+  }
+
+  if (LOGICDEBUG) {
+    Serial.println("Section: " + String(section));
+  }
 
   if (state) {
     bitSet(keyStatus[block], (section * 2));
-  } else {
+  }
+  else {
     bitClear(keyStatus[block], (section * 2));
   }
 }
 
 boolean sentStateForKey(byte key) {
-  byte block = abs(key / 4); // key=87, block = 21
-  byte section = key - (block * 4) - 1; // key = 87, section = 2
+  byte block = byte(abs(key / 4)); // key=87, block = 21
+  byte section;
+
+  if (key > 0) {
+    section = byte(key - (block * 4)); // key = 87, section = 2
+  } else {
+    section = key;
+  }
+
+  if (LOGICDEBUG) {
+    Serial.println("Section: " + String(section));
+  }
 
   return bitRead(keyStatus[block], (section * 2) + 1);
 }
 
 void setSentStateForKey(boolean state, byte key) {
-  byte block = abs(key / 4); // key=87, block = 21
-  byte section = key - (block * 4) - 1; // key = 87, section = 2
+  byte block = byte(abs(key / 4)); // key=87, block = 21
+  byte section;
+
+  if (key > 0) {
+    section = byte(key - (block * 4)); // key = 87, section = 2
+  } else {
+    section = key;
+  }
+
+  if (LOGICDEBUG) {
+    Serial.println("Section: " + String(section));
+  }
 
   if (state) {
     bitSet(keyStatus[block], (section * 2) + 1);
-  } else {
+  }
+  else {
     bitClear(keyStatus[block], (section * 2) + 1);
   }
 }
@@ -163,18 +381,43 @@ boolean downStateForKey(byte key) {
 void setDownStateForKey(boolean state, byte key) {
   if (state) {
     bitSet(keyVelocity[key], 0);
-  } else {
+  }
+  else {
     bitClear(keyVelocity[key], 0);
   }
 }
 
 byte getKeyPosition(byte key) {
   // Shift off the down state and return number
-  return keyVelocity[key] >> 1;
+  return byte(keyVelocity[key] >> 1);
 }
 
 void setKeyPosition(byte key, byte keyPosition) {
-  byte down = keyVelocity[key] & 1; // Get down state from byte
-  keyPosition = keyPosition << 1; // Shift keyPosition to make room for down state
-  keyVelocity[key] == (keyPosition | down); // Merge keyPosition and down state
+  byte down = byte(keyVelocity[key] & 1); // Get down state from byte
+  keyPosition = byte(keyPosition << 1); // Shift keyPosition to make room for down state
+  keyVelocity[key] = byte(keyPosition | down); // Merge keyPosition and down state
+
+  if (LOGICDEBUG) {
+    Serial.println(String(keyVelocity[key]));
+  }
+}
+
+// ====================================================================================
+// Event handlers for incoming MIDI messages
+// ====================================================================================
+
+void OnAppleMidiConnected(char* name) {
+  Serial.print("C: ");
+  Serial.println(name);
+}
+
+void OnAppleMidiDisconnected() {
+  Serial.println("D");
+}
+
+void OnAppleMidiControlChange(byte channel, byte number, byte value) {
+  if (number == 123) {
+    //clearRegisters();
+    delay(20);
+  }
 }
